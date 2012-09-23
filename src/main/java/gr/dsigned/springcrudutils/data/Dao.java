30a3af0a13e1b80f80589cd9d3ddd22add93d8a5
nk <nk@dsigned.gr>
2012-09-23 21:54:40 +0300

    Converted Dao interface to abstract class.

--- src/main/java/gr/dsigned/springcrudutils/data/Dao.java
+++ src/main/java/gr/dsigned/springcrudutils/data/Dao.java
@@ -3,27 +3,27 @@ package gr.dsigned.springcrudutils.data;
 import gr.dsigned.springcrudutils.types.SystemEntity;
 import java.util.List;
 
-public interface Dao<K, E> {
+public abstract class Dao<K, E> {
 
 
 
-    public E persist(E entity);
+    public abstract E persist(E entity);
 
-    public void remove(E entity);
+    public abstract void remove(E entity);
 
-    public E findById(K id);
+    public abstract E findById(K id);
 
-    public void removeById(K id);
+    public abstract void removeById(K id);
 
-    public void merge(E entity);
+    public abstract void merge(E entity);
 
-    public Long countAll(Class<? extends SystemEntity> entity);
+    public abstract Long countAll(Class<? extends SystemEntity> entity);
 
-    public List<E> listAll(Class<? extends SystemEntity> entity, int maxNo, int offset);
+    public abstract List<E> listAll(Class<? extends SystemEntity> entity, int maxNo, int offset);
 
-    public List<E> listAll();
+    public abstract List<E> listAll();
 
-    public SystemEntity findEntityById(Class<? extends SystemEntity> entity, K id);
+    public abstract SystemEntity findEntityById(Class<? extends SystemEntity> entity, K id);
 
 
 }
